65263585667e6a93a3fcb971c8486f3b405309ca
yuy <11335088@dcsh.tp.edu.tw>
2026-01-01 20:27:57 +0800

    登入/登出

--- web/views.py
+++ web/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Message
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
 # 留言列表
 class MessageList(ListView):
@@ -16,7 +17,11 @@ class MessageDetail(DetailView):
     model = Message
     
 # 新增留言
-class MessageCreate(CreateView):
+class MessageCreate(LoginRequiredMixin,CreateView):
     model = Message
     fields = ['user', 'subject', 'content']     # 僅顯示 user, subject, content 這 3 個欄位
     success_url = reverse_lazy('msg_list')                   # 新增成功後，導向留言列表
+
+class MessageDelete(LoginRequiredMixin,DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')               # 刪除成功返回留言列表
